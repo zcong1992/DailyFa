bec6829a8c1ab74eb754c95a0c722734cc535fec
zhangcong <zane0905@163.com>
2018-05-16 17:15:42 +0800

    声明变量，练习

--- app/src/main/java/com/andrzc/dailyfa/KotlinActivity.kt
+++ app/src/main/java/com/andrzc/dailyfa/KotlinActivity.kt
@@ -8,5 +8,38 @@ class KotlinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
+
+        sales()
+        for (x in 1..6) {
+//            println("值 为: $x")
+        }
+
+        val x = 9
+        val y = 10
+        if (x in 1..y - 1) {
+            println("x在范围内 ，值为$x")
+        }
+
+        cases("hello")
+        cases(1)
     }
+
+    fun sales() {
+        var quantity = 5;
+        val price = 20.3
+        val name: String = "大米"
+
+        println("单价：$price")
+        println("数量：$quantity")
+        println("产品 $name 总计：${quantity * price}")
+    }
+
+    fun cases(obj: Any) {
+        when (obj) {
+            1 -> println("这是第一项")
+            "hello" -> println("这是一个hello字符串")
+            is Long -> println("这是一个Long类型数据")
+        }
+    }
+
 }
---
+++ app/src/main/java/com/andrzc/dailyfa/kotlin/HelloKotlin.kt
@@ -0,0 +1,10 @@
+package com.andrzc.dailyfa.kotlin
+
+/**
+ * Created by ZC on 2018/05/16.
+ */
+
+fun main(args: String) {
+
+    print("Hello Kotlin")
+}
